35e952c17d5fd98f6378f456bde0233edb3cf336
Игорь Кочугин <igor.kochugin>
2020-10-23 23:02:18 +0300

    Serve static files only in development mode

--- app.rb
+++ app.rb
@@ -6,12 +6,19 @@ require 'roda'
 class App < Roda
     # Specifying the path to the root of the application
     opts[:root] = __dir__ # Here it is write that the root of the app is that file
-    # Serving static files
-    plugin :public
+    # Allows you to customize the application depending on the environment in which it was launched
+    plugin :environments
+
+    # Development environment only
+    configure :development do
+      # Serving static files
+      plugin :public
+      opts[:serve_static] = true
+    end
 
   route do |r|
     # All requests would be processed
-    r.public
+    r.public if opts[:serve_static]
 
     r.root do
       'Hello, World!'
