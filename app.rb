f7fae51a13b90623e8c9fadf4ec28b092a4fb73a
Игорь Кочугин <igor.kochugin>
2020-10-23 22:50:07 +0300

    Setup static files delivery and add foundation templat

--- app.rb
+++ app.rb
@@ -4,7 +4,15 @@ require 'roda'
 
 # Core class
 class App < Roda
+    # Specifying the path to the root of the application
+    opts[:root] = __dir__ # Here it is write that the root of the app is that file
+    # Serving static files
+    plugin :public
+
   route do |r|
+    # All requests would be processed
+    r.public
+
     r.root do
       'Hello, World!'
     end
